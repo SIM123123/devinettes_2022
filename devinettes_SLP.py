90a59be98884a72baeaa2aa6ba46a2921f82d2f7
SIM123123 <95240563+SIM123123@users.noreply.github.com>
2022-11-07 14:10:44 -0500

    Début devinette

---
+++ devinettes_SLP.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Jeu de devinettes.
+par Simon Lamarche Perrea
+"""
+
+def main():
+    """
+    Fonction principale
+    """
+    print("SLP: Jeu de devinette...")
+
+
+if __name__ == '__main__':
+    main()
+
+
